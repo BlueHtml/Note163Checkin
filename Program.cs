183629e101bbd1e10dd577f7c9e95deeffe3ef10
fly <luxiaofeng006@qq.com>
2020-09-16 16:45:32 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Note163Checkin
+{
+    class Program
+    {
+        static Conf _conf;
+        static HttpClient _scClient;
+
+        static async Task Main()
+        {
+            _conf = Deserialize<Conf>(GetEnvValue("CONF"));
+            if (!string.IsNullOrWhiteSpace(_conf.ScKey))
+            {
+                _scClient = new HttpClient();
+            }
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "ynote-android");
+            Console.WriteLine("有道云笔记签到开始运行...");
+
+            for (int i = 0; i < _conf.Users.Length; i++)
+            {
+                User user = _conf.Users[i];
+                string title = $" 账号 {i + 1}: {user.Name} ";
+                Console.WriteLine($"共 {_conf.Users.Length} 个账号，正在运行{title}...");
+
+                client.DefaultRequestHeaders.Remove("Cookie");
+                client.DefaultRequestHeaders.Add("Cookie", user.Cookie);
+
+                //每日打开客户端（即登陆）
+                string result = await (await client.PostAsync("https://note.youdao.com/yws/api/daupromotion?method=sync", null))
+                    .Content.ReadAsStringAsync();
+                if (result.Contains("error", StringComparison.OrdinalIgnoreCase))
+                {//Cookie失效
+                    await Notify($"{title}Cookie失效，请及时更新！", true);
+                    continue;
+                }
+
+                long space = 0;
+                space += Deserialize<YdNoteRsp>(result).RewardSpace;
+
+                //签到
+                result = await (await client.PostAsync("https://note.youdao.com/yws/mapi/user?method=checkin", null))
+                   .Content.ReadAsStringAsync();
+                space += Deserialize<YdNoteRsp>(result).Space;
+
+                //看广告
+                for (int j = 0; j < 3; j++)
+                {
+                    result = await (await client.PostAsync("https://note.youdao.com/yws/mapi/user?method=adPrompt", null))
+                       .Content.ReadAsStringAsync();
+                    space += Deserialize<YdNoteRsp>(result).Space;
+                }
+
+                //看视频广告
+                for (int j = 0; j < 3; j++)
+                {
+                    result = await (await client.PostAsync("https://note.youdao.com/yws/mapi/user?method=adRandomPrompt", null))
+                       .Content.ReadAsStringAsync();
+                    space += Deserialize<YdNoteRsp>(result).Space;
+                }
+
+                await Notify($"有道云笔记{title}签到成功，共获得空间 {space / 1048576} M");
+            }
+            Console.WriteLine("签到运行完毕");
+        }
+
+        static async Task Notify(string msg, bool isFailed = false)
+        {
+            Console.WriteLine(msg);
+            if (_conf.ScType == "Always" || (isFailed && _conf.ScType == "Failed"))
+            {
+                await _scClient?.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?text={msg}");
+            }
+        }
+
+        static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            ReadCommentHandling = JsonCommentHandling.Skip
+        };
+        static T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json, _options);
+
+        static string GetEnvValue(string key) => Environment.GetEnvironmentVariable(key);
+    }
+
+    #region Conf
+
+    public class Conf
+    {
+        public User[] Users { get; set; }
+        public string ScKey { get; set; }
+        public string ScType { get; set; }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Cookie { get; set; }
+    }
+
+    #endregion
+
+    class YdNoteRsp
+    {
+        /// <summary>
+        /// Sync奖励空间
+        /// </summary>
+        public int RewardSpace { get; set; }
+
+        /// <summary>
+        /// 其他奖励空间
+        /// </summary>
+        public int Space { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,61 @@
+# Note163Checkin
+
+**并非全自动，需要使用Fiddler或其他抓包工具获取到Cookie**
+
+## 一、Fork 仓库
+
+**点击右上角的`Fork`**
+![Fork](https://img.guoqianfan.com/note/2020/08/fork.png)
+
+## 二、添加 Secret
+
+**`Settings`->`Secrets`->`New secret`，添加以下Secret：**
+- `Conf`：其值如下：
+    ```json
+    {
+    	"Users": [{
+    			"Name": "CC", //自定义名字，选填
+    			"Cookie": "YNOTE_LOGIN=true; YNOTE_SESS=v2|abc" //有道云笔记客户端抓包的cookie
+    		}, {
+    			"Name": "MM",
+    			"Cookie": "YNOTE_LOGIN=true; YNOTE_SESS=v2|123"
+    		}
+    	],
+    	"ScKey": "", //server酱sckey，不填不开启
+    	"ScType": "Failed" //通知类型. Always:始终通知; Failed:失败时通知; 不填/其他:不通知;
+    }
+    ```
+
+**步骤图示如下：**
+![添加Secret](https://img.guoqianfan.com/note/2020/08/添加secret.png)
+
+## 三、启用 Action
+
+**点击`Actions`，再点击`I understand my workflows, go ahead and enable them`**
+![启用Action](https://img.guoqianfan.com/note/2020/08/启用action.png)
+
+**注意**：Fork 的仓库上的 GitHub Actions 的**定时任务不会自动执行**，必须要手动触发一次后才能正常工作。
+
+所以 Fork 之后，点击自己仓库右上角的`Star`，`Star`你的仓库，这是为了触发 Github Action 第一次执行，之后就会自动执行定时任务。
+![Star](https://img.guoqianfan.com/note/2020/08/star.png)
+
+## 四、查看运行结果
+
+**`Actions`->`Checkin`->`build`**，能看到下图，表示运行成功（注意：由于 .NET Core会输出默认日志，请**滚动到最下面查看实际运行结果**）
+![查看Action运行记录](https://img.guoqianfan.com/note/2020/08/查看action运行记录.png)
+
+## 注意事项
+
+并非全自动，需要使用Fiddler或其他抓包工具获取到Cookie。
+
+每天运行一次，在上午9:00-9:45之间。
+
+也可以点击右上角的`Star`手动运行。
+![Star](https://img.guoqianfan.com/note/2020/08/star.png)
+
+## 参考
+
+参考了以下项目：
+- [ydao](https://github.com/yygtboy/ydao/)
+- [node-script](https://github.com/SunSeekerX/node-script)
+- [youdaoyun](https://github.com/hezhizheng/youdaoyun)
